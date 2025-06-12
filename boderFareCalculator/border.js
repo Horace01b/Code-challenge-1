ee56cf3f1df34d8de1262c6551e6acfb941fe558
horaceo1b <horaceken0@gmail.com>
2025-06-12 14:03:07 +0300

    initial commit

--- boderFareCalculator/border.js
+++ boderFareCalculator/border.js
@@ -1,15 +1,17 @@
-function calculateBodaFare(distanceInKm) {
+function calculateBodaFare() {
 const input = prompt('Unafika wapi Mkubwa? Kilometer ngapi?');
- distanceinkilometers = parseInt(input);
-if(isNaN(distanceinkilometers) || distanceinkilometers >0) {
-    calculateBodaFare(distanceinkilometers); 
+const distanceinkilometers = parseInt(input);
+if(isNaN(distanceinkilometers) || distanceinkilometers <=0) {
+    alert("Please enter a valid number greater than 0");
+    return calculateBodaFare() 
 }
 
 const baseFare = 50; 
 const chargePerKm = 15;
-const totalFare = baseFare + (distanceInKm * chargePerKm);
+const fareDistance = distanceInKm * chargePerKm
+const totalFare = baseFare + fareDistance;
 
-console.log('For a ${distnceInKm} km Boda Boda ride:');
+console.log(`For a ${distanceInKm} km Boda Boda ride:`);
 console.log('- Base Fare: KES ${baseFare');
 console.log('- Distance Fare: KES ${fareDistance} (KES 15 * ${distanceInKm} km)');
 console.log('=> Total Estimated Fare: KES ${totalFare}');
