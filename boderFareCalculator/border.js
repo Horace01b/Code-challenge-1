2d352c3b15fe5c008cdfeb842275d103ce398b9b
horaceo1b <horaceken0@gmail.com>
2025-06-17 23:19:11 +0300

    initial commit

--- boderFareCalculator/border.js
+++ boderFareCalculator/border.js
@@ -1,21 +1,29 @@
-function calculateBodaFare() {
-const input = prompt('Unafika wapi Mkubwa? Kilometer ngapi?');
-const distanceinkilometers = parseInt(input);
-if(isNaN(distanceinkilometers) || distanceinkilometers <=0) {
-    alert("Please enter a valid number greater than 0");
-    return calculateBodaFare() 
+function calculateBodaFare(){
+    const input = prompt('Unafika wapi Mkubwa? Kilometer ngapi?');
+    const distanceInKm = parseInt(input);
+
+
+if (isNaN(distanceInKm || distanceInKm <= 0)) {
+    alert("Please enter a number greater than 0");
+    return calculateBodaFare();
 }
 
-const baseFare = 50; 
+const baseFare = 50;
 const chargePerKm = 15;
-const fareDistance = distanceInKm * chargePerKm
+const fareDistance = distanceInKm * chargePerKm;
 const totalFare = baseFare + fareDistance;
 
-console.log(`For a ${distanceInKm} km Boda Boda ride:`);
-console.log('- Base Fare: KES ${baseFare');
-console.log('- Distance Fare: KES ${fareDistance} (KES 15 * ${distanceInKm} km)');
-console.log('=> Total Estimated Fare: KES ${totalFare}');
+console.log(` For a ${distanceInKm} km Boda Boda ride:`);
+console.log(`- Base Fare KES ${baseFare}`);
+console.log(`- Distance Fare: KES ${fareDistance} (KES 15 * ${distanceInKm} km)`);
+console.log(` => Total Estimated Fare: KES ${totalFare}`);
 
+alert(`Boda Boda Fare Summary\n
+    distance: ${distanceInKm} km\n
+    baseFare: KES ${baseFare}\n
+    fareDistance: KES ${fareDistance} (${chargePerKm} * ${distanceInKm})\n
+    totalFare: KES ${totalFare}`);
 
 }
-calculateBodaFare()
+
+calculateBodaFare();
